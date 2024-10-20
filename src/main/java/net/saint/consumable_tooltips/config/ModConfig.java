f15ac082b2067839167693f55d83157260cef703
August Saint Freytag <me@augustfreytag.com>
2024-10-20 11:35:55 +0200

    Establish mod config, load hook, add localization

---
+++ src/main/java/net/saint/consumable_tooltips/config/ModConfig.java
@@ -0,0 +1,22 @@
+package net.saint.consumable_tooltips.config;
+
+import me.shedaniel.autoconfig.ConfigData;
+import me.shedaniel.autoconfig.annotation.Config;
+import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.Comment;
+
+@Config(name = "consumable_tooltips")
+public class ModConfig implements ConfigData {
+
+	@Comment("Enable health rendering in tooltip.")
+	public boolean enableHealthTooltip = true;
+
+	@Comment("Enable satiety rendering in tooltip.")
+	public boolean enableSatietyTooltip = true;
+
+	@Comment("Enable saturation rendering in tooltip.")
+	public boolean enableSaturationTooltip = true;
+
+	@Comment("Enable hydration rendering in tooltip.")
+	public boolean enableHydrationTooltip = true;
+
+}
