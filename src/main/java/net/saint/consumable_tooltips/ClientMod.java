896e25826f888f5570eec0a9778ffb25bf362db1
August Saint Freytag <me@augustfreytag.com>
2024-10-25 23:48:56 +0200

    Add tooltip phase order rules for Farmer's Delight
    Ensure Farmer's Delight inserted tooltips show above mod tooltips.

--- src/main/java/net/saint/consumable_tooltips/ClientMod.java
+++ src/main/java/net/saint/consumable_tooltips/ClientMod.java
@@ -2,15 +2,19 @@ package net.saint.consumable_tooltips;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
+import net.minecraft.util.Identifier;
 import net.saint.consumable_tooltips.util.TooltipUtil;
 
 public class ClientMod implements ClientModInitializer {
 
 	@Override
 	public void onInitializeClient() {
-		ItemTooltipCallback.EVENT.register((stack, context, lines) -> {
+		ItemTooltipCallback.EVENT.register(Mod.IDENTIFIER, (stack, context, lines) -> {
 			TooltipUtil.addTooltip(stack, context, lines);
 		});
+
+		var farmersDelightIdentifier = new Identifier("farmersdelight");
+		ItemTooltipCallback.EVENT.addPhaseOrdering(farmersDelightIdentifier, Mod.IDENTIFIER);
 	}
 
 }
