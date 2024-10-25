6b50974c3d6697cbb0e279b56a67f7078667ec1f
August Saint Freytag <me@augustfreytag.com>
2024-10-25 23:45:06 +0200

    Rewrite Capsaicin access, use new method
    Use new strategy to access modified food properties via
    food handler instance and food components.

--- src/main/java/net/saint/consumable_tooltips/util/CapsaicinAccess.java
+++ src/main/java/net/saint/consumable_tooltips/util/CapsaicinAccess.java
@@ -5,11 +5,10 @@ import java.util.ArrayList;
 import de.siphalor.capsaicin.api.food.FoodModifications;
 import de.siphalor.capsaicin.api.food.FoodProperties;
 import de.siphalor.capsaicin.impl.food.FoodContextImpl;
+import de.siphalor.capsaicin.impl.food.FoodHandler;
 import de.siphalor.capsaicin.impl.food.properties.FoodPropertiesImpl;
-import de.siphalor.capsaicin.impl.util.IItem;
 import net.dehydration.Mod;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.FoodComponent;
 import net.minecraft.item.ItemStack;
 import net.saint.consumable_tooltips.library.ItemFoodProperties;
 
@@ -18,28 +17,25 @@ public class CapsaicinAccess {
 	private static final boolean USE_APPLE_SKIN_LIKE_ACCESS = true;
 
 	public ItemFoodProperties getModifiedFoodPropertiesForStack(ItemStack stack, PlayerEntity player) {
+		if (USE_APPLE_SKIN_LIKE_ACCESS) {
+			return getPropertiesViaAppleSkinLike(stack, player);
+		} else {
+			return getPropertiesViaFoodModifications(stack, player);
+		}
+	}
+
+	private ItemFoodProperties getPropertiesViaFoodModifications(ItemStack stack, PlayerEntity player) {
 		var defaultFoodComponent = stack.getItem().getFoodComponent();
 
 		if (defaultFoodComponent == null) {
 			return null;
 		}
 
-		if (USE_APPLE_SKIN_LIKE_ACCESS) {
-			return getPropertiesViaAppleSkinLike(stack, player, defaultFoodComponent);
-		} else {
-			return getPropertiesViaFoodModifications(stack, player, defaultFoodComponent);
-		}
-	}
-
-	private ItemFoodProperties getPropertiesViaFoodModifications(ItemStack stack, PlayerEntity player,
-			FoodComponent defaultFoodComponent) {
 		// Original
-
 		FoodProperties originalFoodProperties = new FoodPropertiesImpl(defaultFoodComponent.getHunger(),
 				defaultFoodComponent.getSaturationModifier(), false, new ArrayList<>());
 
 		// Modified
-
 		FoodProperties modifiedFoodProperties = new FoodPropertiesImpl(defaultFoodComponent.getHunger(),
 				defaultFoodComponent.getSaturationModifier(), false, new ArrayList<>());
 		FoodModifications.PROPERTIES_MODIFIERS.apply(modifiedFoodProperties,
@@ -49,24 +45,21 @@ public class CapsaicinAccess {
 		return ItemFoodProperties.fromFoodPropertiesTuple(originalFoodProperties, modifiedFoodProperties);
 	}
 
-	private ItemFoodProperties getPropertiesViaAppleSkinLike(ItemStack stack, PlayerEntity player,
-			FoodComponent defaultFoodComponent) {
-		var item = stack.getItem();
+	private ItemFoodProperties getPropertiesViaAppleSkinLike(ItemStack stack, PlayerEntity player) {
+		var foodHandler = FoodHandler.INSTANCE.get();
+		foodHandler.withUser(player).withStack(stack);
 
-		if (!(item instanceof IItem iitem)) {
-			// Fallback, can not use AppleSkin-like access.
-			// return new ItemFoodProperties(originalFoodComponents,
-			// originalFoodComponents);
-			Mod.LOGGER.warn("Could not read modified food properties (via AppleSkin-like) for item '" + item + "'.");
-			return ItemFoodProperties.fromDefaultFoodComponent(defaultFoodComponent);
+		var defaultFoodComponent = foodHandler.getStackOriginalFoodComponent();
+
+		if (defaultFoodComponent == null) {
+			return null;
 		}
 
-		var modifiedFoodComponent = iitem.capsaicin$getVanillaFoodComponent();
+		var modifiedFoodComponent = foodHandler.getModifiedFoodComponent();
 
 		if (modifiedFoodComponent == null) {
-			// Fallback, used AppleSkin-like access but could not get valid properties.
-			// This may mean that the item's properties were not modified.
-			Mod.LOGGER.warn("Read modified food properties (via AppleSkin-like) are null for item '" + item + "'.");
+			Mod.LOGGER.warn("Could not get modified food component alongside default for stack: '{}'",
+					stack.getTranslationKey());
 			return ItemFoodProperties.fromDefaultFoodComponent(defaultFoodComponent);
 		}
 
